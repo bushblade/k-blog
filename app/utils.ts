6077132df4f5ee65c41c3ae111d04476c258906a
Will Adams <bushblade@gmail.com>
2022-07-07 16:12:13 +0100

    day suffix

--- app/utils.ts
+++ app/utils.ts
@@ -33,7 +33,18 @@ const dateFormatter = new Intl.DateTimeFormat('en-GB', { dateStyle: 'long' })
  */
 
 export function formatDate(dateStr: string): string {
-  return dateFormatter.format(new Date(dateStr))
+  const formattedDate = dateFormatter.format(new Date(dateStr))
+  const [day, ...rest] = formattedDate.split(' ')
+  const lastNum = day[day.length - 1]
+  const suffix =
+    lastNum === '1'
+      ? 'st'
+      : lastNum === '2'
+      ? 'nd'
+      : lastNum === '3'
+      ? 'rd'
+      : 'th'
+  return [`${day}${suffix}`, ...rest].join(' ')
 }
 
 export function lessThanNdaysOld(dateStr: string, n: number = 30): boolean {
