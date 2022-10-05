8bcaef6e6ea9c8f0351ace518efb47b6db14ad89
Will Adams <bushblade@gmail.com>
2022-10-05 17:10:26 +0100

    dynamic data tip greeting

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,18 +1,27 @@
+import { useState } from 'react'
 import type { Author } from '~/graphql/graphcmsTypes'
 import Header from './Header'
 
-// function randomGreeting() {
-//   const greetings = ['Hey there!', 'Hi!', "Yep that's me!"]
-//   return greetings[Math.floor(Math.random() * greetings.length + 1)]
-// }
+const greetings = ['Hey there!', 'Hi!', `Yep that's me!`]
+
+function randomGreeting() {
+  return greetings[Math.floor(Math.random() * greetings.length)]
+}
 
 export default function Banner({ author }: { author: Author }) {
+  const [greeting, setGreeting] = useState(greetings[0])
   return (
     <Header>
       <div className='max-w-md'>
         <div
           className='avatar tooltip tooltip-left tooltip-primary'
-          data-tip="Yep that's me!"
+          data-tip={greeting}
+          onMouseLeave={() => {
+            const timeoutId = setTimeout(() => {
+              setGreeting(randomGreeting())
+              clearTimeout(timeoutId)
+            }, 300)
+          }}
         >
           <div className='w-28 rounded-full ring ring-primary'>
             <img src={author.picture?.url} alt={author.name} />
@@ -20,7 +29,7 @@ export default function Banner({ author }: { author: Author }) {
         </div>
         <h1 className='text-5xl'>Hi I&apos;m {author.name}</h1>
         <p className='pt-6 font-bold'>A {author.title}</p>
-        <p className='pb-6'>Welcome to my website</p>
+        <p className='pb-6'>{author.biography}</p>
       </div>
     </Header>
   )
