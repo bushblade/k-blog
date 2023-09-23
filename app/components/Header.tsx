16f5074805da76f88938d9229f16ddfd20bf08c1
Will Adams <bushblade@gmail.com>
2023-09-23 19:58:57 +0100

    min size for header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 import StackedWave from './StackedWave'
 export default function Header({ children }: { children: React.ReactNode }) {
   return (
-    <header className='hero py-8 bg-gradient-to-tl from-base-300 to-base-200 relative min-h-[360px]'>
+    <header className='hero py-8 bg-gradient-to-tl from-base-300 to-base-200 relative min-h-[386px]'>
       <StackedWave />
       <div className='text-center hero-content flex content-center align-center'>
         {children}
